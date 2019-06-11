4509c60e52f40a6d7066b746558e7ef6db72ec8a
LEARNET\osama.alnouri1 <m.ahmad1999@hotmail.com>
2019-06-11 14:31:46 +0200

    v1.0.3

--- SmartSoft/Pages/Owner.aspx.cs
+++ SmartSoft/Pages/Owner.aspx.cs
@@ -13,7 +13,6 @@ namespace SmartSoft.Pages
     public partial class Owner : System.Web.UI.Page
     {
          
-        SqlConnection con = new SqlConnection(ConnectionString.con);
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -21,20 +20,9 @@ namespace SmartSoft.Pages
 
         protected void btn_add_Click(object sender, EventArgs e)
         {
-            if (con.State == ConnectionState.Closed)
-                con.Open();
-            SqlCommand sql = new SqlCommand("ADDADMIN", con);
-            sql.CommandType = CommandType.StoredProcedure;
-            sql.Parameters.AddWithValue("@ID", (_AdminId.Value == ""?0:Convert.ToInt32(_AdminId.Value)));
-            sql.Parameters.AddWithValue("@Username",username.Text.Trim());
-            sql.Parameters.AddWithValue("@Password", password.Text.Trim());
-            sql.Parameters.AddWithValue("@UType", 1);
-            sql.Parameters.AddWithValue("@School", school.Text.Trim());
-            sql.ExecuteNonQuery();
-            con.Close();
+            Main.AddTo(Producer.AddAccount, new string[] { "ID","Username","Password",
+            "UType","School"}, 0, username.Text, password.Text, 1, school.Text);
             Clear();
-             
-             
         }
 
         void Clear()
